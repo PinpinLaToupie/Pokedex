7133e598ef1e42433faa8d7945ed7ba9b2aae0e5
clément féménias <femenias.c@gmail.com>
2023-10-10 11:54:02 +0200

    second commit

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,9 +1,29 @@
 function PokemonCard() {
+
+    const pokemon = pokemonList[0];
+
     return (
         <figure>
+            {pokemon.imgSrc ? (
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            ) : (
+            <p>???</p>
+            )}
             <figcaption>Bulbasaur</figcaption>
         </figure>
     );
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+
 export default PokemonCard;
